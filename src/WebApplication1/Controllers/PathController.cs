3b9e68601bbc5226bf9ddefddd4a19929e60f7bf
ingAlfano <ing.alessandro.alfano@gmail.com>
2016-07-27 11:23:07 +0200

    First commit

---
+++ src/WebApplication1/Controllers/PathController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class PathController : Controller
+    {
+        // GET api/path/myPath
+        [HttpGet("{*fullPath}")]
+        public async Task<IActionResult> Get([FromQuery]string fullPath = "defaultValue")
+        {
+            return Ok(fullPath);
+        }
+    }
+}
